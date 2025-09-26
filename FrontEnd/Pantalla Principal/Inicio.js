e85ca66356a8a8379273cbbc46f6bdca87a4b2f2
DanteBotta <dantegiovannibotta@gmail.com>
2025-09-26 17:56:49 -0300

    Pantalla Inicio Terminada, falta linkear el boton de colección

---
+++ FrontEnd/Pantalla Principal/Inicio.js
@@ -0,0 +1,18 @@
+//Boton que cambia el fondo
+let cuadrado = document.getElementById("cuadrado")
+let Fondo = false
+cuadrado.addEventListener("click", function(){
+    if (Fondo){
+        document.body.style.backgroundImage = 'url("img/fondo2.png")'
+        Fondo = false
+    }
+    else{
+        document.body.style.backgroundImage = 'url("img/fondo.png")'
+        Fondo = true
+    }
+})
+8
+let Jugar = document.getElementById("Jugar")
+Jugar.addEventListener("click", function(){
+    window.location.href = "../Pantalla Juego/Papaleo.html"
+})
